b23278e41e07e1771f68acaa4809dab5644f5262
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-07-06 13:22:41 +0200

    Fix Rails schema compatibility

--- db/migrate/20251212154340_add_singleton_constraint_to_accounts.rb
+++ db/migrate/20251212154340_add_singleton_constraint_to_accounts.rb
@@ -1,4 +1,4 @@
-class AddSingletonConstraintToAccounts < ActiveRecord::Migration[8.2]
+class AddSingletonConstraintToAccounts < ActiveRecord::Migration[8.1]
   def change
     add_column :accounts, :singleton_guard, :integer, default: 0, null: false
     add_index :accounts, :singleton_guard, unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.2].define(version: 2025_12_12_154340) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_12_154340) do
   create_table "accounts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "custom_styles"
